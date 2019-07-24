c8aec068784d45137dce0d43ed9fe6a9caad2764
VictorParodi <victor.parodi.montero@gmail.com>
2019-07-24 00:29:08 -0500

    app1 commit 1: setting up the routes for streams components

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-
-const pageOne = () => {
-    return (
-        <div>
-            <h1>Page One</h1>
-            <Link to="/pagetwo">To page two</Link>
-        </div>
-    );
-};
-
-const pageTwo = () => {
-    return (
-        <div>
-            <h1>Page Two</h1>
-            <Link to="/">To page one</Link>
-        </div>
-    );
-};
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamList from './streams/StreamList.js';
+import StreamCreate from './streams/StreamCreate.js';
+import StreamEdit from './streams/StreamEdit.js';
+import StreamShow from './streams/StreamShow.js';
+import StreamDelete from './streams/StreamDelete.js';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={pageOne} />
-                    <Route path="/pagetwo" component={pageTwo} />
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" component={StreamCreate}/>
+                    <Route path="/streams/edit" component={StreamEdit}/>
+                    <Route path="/streams/show" component={StreamShow}/>
+                    <Route path="/streams/delete" component={StreamDelete}/>
                 </div>
             </BrowserRouter>
         </div>
