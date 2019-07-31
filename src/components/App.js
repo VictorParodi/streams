96ca5e1d93f6397c55a1f490aaf5166a19847a9c
VictorParodi <victor.parodi.montero@gmail.com>
2019-07-30 20:15:55 -0500

    app1 commit 2: initializing the Google API for auth

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import StreamCreate from './streams/StreamCreate.js';
 import StreamEdit from './streams/StreamEdit.js';
 import StreamShow from './streams/StreamShow.js';
 import StreamDelete from './streams/StreamDelete.js';
+import Header from './Header.js';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route path="/" exact component={StreamList}/>
                     <Route path="/streams/new" component={StreamCreate}/>
                     <Route path="/streams/edit" component={StreamEdit}/>
