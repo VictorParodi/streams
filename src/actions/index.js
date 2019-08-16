b456ffdd46ccf5fa82ce4e09af75d42065c71a2d
VictorParodi <victor.parodi.montero@gmail.com>
2019-08-16 00:08:01 -0500

    app1 commit 6: using constants for type, and looking for the current user logged in

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
-export const signIn = () => {
-    return {type: 'SIGN_IN'};
+import {SIGN_IN, SIGN_OUT} from './types.js';
+
+export const signIn = (userId) => {
+    return {type: SIGN_IN, payload: userId};
 };
 
 export const signOut = () => {
-    return {type: 'SIGN_OUT'};
+    return {type: SIGN_OUT};
 };
