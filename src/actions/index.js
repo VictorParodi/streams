0d0e9585fb2924535cb0f516aa21ca776b6ca462
VictorParodi <victor.parodi.montero@gmail.com>
2019-08-28 23:15:06 -0500

    app1 commit 12: creating an action creator for creating streams

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import {SIGN_IN, SIGN_OUT} from './types.js';
+import streams from './../apis/streams.js';
 
 export const signIn = (userId) => {
     return {type: SIGN_IN, payload: userId};
@@ -7,3 +8,9 @@ export const signIn = (userId) => {
 export const signOut = () => {
     return {type: SIGN_OUT};
 };
+
+export const createStream = (formValues) => {
+    return async (dispatch, getState) => {
+        streams.post('/streams', formValues);
+    }
+}
