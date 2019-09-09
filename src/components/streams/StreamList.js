41ec6d8f8a1cb3befc6210c98d088c1cf37f48e1
VictorParodi <victor.parodi.montero@gmail.com>
2019-09-08 22:07:53 -0500

    app1 commit 19: showing admin buttons for the current user

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -7,6 +7,22 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin = (stream) => {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui primary button">
+                        Edit
+                    </button>
+
+                    <button className="ui negative button">
+                        Delete
+                    </button>
+                </div>
+            );
+        }
+    }
+
     renderList = () => {
         return this.props.streams.map((stream) => {
             return (
@@ -16,6 +32,7 @@ class StreamList extends React.Component {
                         {stream.title}
                         <div className="description">{stream.description}</div>
                     </div>
+                    {this.renderAdmin(stream)}
                 </div>
             );
         });
@@ -27,7 +44,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return {streams: Object.values(state.streams)};
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    };
 }
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
