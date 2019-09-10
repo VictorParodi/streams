454188a8b349d5517a55e8d7ff9f203216a93c88
VictorParodi <victor.parodi.montero@gmail.com>
2019-09-09 20:20:12 -0500

    app1 commit 20: applying the custom history object

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import StreamList from './streams/StreamList.js';
 import StreamCreate from './streams/StreamCreate.js';
 import StreamEdit from './streams/StreamEdit.js';
 import StreamShow from './streams/StreamShow.js';
 import StreamDelete from './streams/StreamDelete.js';
+import history from './../history.js';
 import Header from './Header.js';
 
 const App = () => {
     return (
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList}/>
@@ -19,7 +20,7 @@ const App = () => {
                     <Route path="/streams/show" component={StreamShow}/>
                     <Route path="/streams/delete" component={StreamDelete}/>
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
