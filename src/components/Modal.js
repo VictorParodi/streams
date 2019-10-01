870d0f725263d2eb61c2d15b46fc27e68f2005f3
VictorParodi <victor.parodi.montero@gmail.com>
2019-09-30 20:36:04 -0500

    app1 commit 29: Improve the style and behavior of the Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from './../history.js';
 
 class Modal extends React.Component {
     render() {
         return ReactDOM.createPortal(
-            <div className="ui dimmer modals visible active">
-                <div className="ui standard modal visible active">
-                    Modal content
+            <div
+                onClick={() => history.push('/')}
+                className="ui dimmer modals visible active"
+            >
+                <div
+                    onClick={(event) => event.stopPropagation()}
+                    className="ui standard modal visible active"
+                >
+                    <div className="header">Delete Stream</div>
+                    <div className="content">
+                        Are you sure you want delete the stream?
+                    </div>
+                    <div className="actions">
+                        <button className="ui primary button">Delete</button>
+                        <button className="ui button">Cancel</button>
+                    </div>
                 </div>
             </div>,
             document.querySelector('#modal')
