d1313e847a8a9924a794b6b9ed58901680f48c75
VictorParodi <victor.parodi.montero@gmail.com>
2019-09-30 21:33:21 -0500

    app1 commit 30: using props system on Modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from './../history.js';
 
 class Modal extends React.Component {
     render() {
         return ReactDOM.createPortal(
             <div
-                onClick={() => history.push('/')}
+                onClick={this.props.onDismiss}
                 className="ui dimmer modals visible active"
             >
                 <div
                     onClick={(event) => event.stopPropagation()}
                     className="ui standard modal visible active"
                 >
-                    <div className="header">Delete Stream</div>
-                    <div className="content">
-                        Are you sure you want delete the stream?
-                    </div>
-                    <div className="actions">
-                        <button className="ui primary button">Delete</button>
-                        <button className="ui button">Cancel</button>
-                    </div>
+                    <div className="header">{this.props.title}</div>
+                    <div className="content">{this.props.content}</div>
+                    <div className="actions">{this.props.actions}</div>
                 </div>
             </div>,
             document.querySelector('#modal')
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Modal from './../Modal.js';
+import history from './../../history.js';
 
 const StreamDelete = () => {
+    const actions = (
+        <React.Fragment>
+            <button className="ui negative button">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
+
     return (
         <div>
             StreamDelete
-            <Modal/>
+            <Modal
+                title="Delete Streams"
+                content="Are you sure you want delete the stream?"
+                actions={actions}
+                onDismiss={() => history.push('/')}
+            />
         </div>
     );
 };
